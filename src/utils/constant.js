25e41ea6db707ee423eebcaa56a7cd28da20a3a3
Ankit Srivastav <your.email@example.com>
2025-02-03 18:23:23 +0530

    chat code updated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-// const API_END_POINT = "https://workify-backend-ddhs.onrender.com/api/v1"
- const API_END_POINT = "http://localhost:5000/api/v1"
+const API_END_POINT = "https://workify-backend-ddhs.onrender.com/api/v1"
+//  const API_END_POINT = "http://localhost:5000/apgiti/v1"
 
 export { API_END_POINT }; 
